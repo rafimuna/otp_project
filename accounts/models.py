eeab09781386a0356adae5f14332f2d37ee71791
rafimuna <rafimuna77@gmail.com>
2025-06-22 13:03:08 +0600

    updated accounts app for phone number login

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class UserManager(BaseUserManager):
@@ -10,9 +10,19 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
+    def create_superuser(self, phone, password):
+        if not phone:
+            raise ValueError("Phone number required")
+        user = self.create_user(phone, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
     phone = models.CharField(max_length=15, unique=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # admin panel access control
 
     USERNAME_FIELD = 'phone'
 
